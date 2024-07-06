835c9dfa4125513a5726b1852f21b33e0358a27b
Aman Kumar <97329195+amanksh05@users.noreply.github.com>
2024-07-07 01:11:25 +0530

    Folder structure created

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const cors = require('cors')
+const { db } = require('./db/db')
+const {readdirSync} = require('fs')
+const { router } = require('./routes/transaction')
+const app = express()
+
+
+
+require('dotenv').config()
+const PORT = process.env.PORT
+
+// Middleware
+
+app.use(express.json())
+app.use(cors())
+
+
+// routes
+readdirSync('./routes').map((route)=>app.use('/api/v1',require(`./routes/${route}`)))
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+const server = ()=>{
+    db()
+    app.listen(PORT,()=>{
+        console.log("You are listening to port : ",PORT);
+    })
+}
+
+server()
